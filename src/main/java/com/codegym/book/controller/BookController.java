c7471d9af6ed204ffb7daeee5df828a211900415
hien <hien07072001@gmail.com>
2020-06-07 20:49:40 +0700

    edit

--- src/main/java/com/codegym/book/controller/BookController.java
+++ src/main/java/com/codegym/book/controller/BookController.java
@@ -22,8 +22,20 @@ public class BookController {
         return new ResponseEntity<>(bookList, HttpStatus.OK);
 
     }
+//        //ResponseEntity: Đại diện cho toàn bộ phản hồi http.
     @PostMapping("/api/books")
     public ResponseEntity<Void>addBook(@RequestBody Book book){
+        bookService.save(book);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Void> DeleteBook(@PathVariable Long id ){
+        bookService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> updateBook(@RequestBody Book book){
         bookService.save(book);
         return new ResponseEntity<>(HttpStatus.OK);
 
