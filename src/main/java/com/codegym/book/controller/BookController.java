945e69a69d47ba272cad53e33dad9259d480f795
hien <hien07072001@gmail.com>
2020-06-07 20:00:29 +0700

    add books

--- src/main/java/com/codegym/book/controller/BookController.java
+++ src/main/java/com/codegym/book/controller/BookController.java
@@ -5,9 +5,7 @@ import com.codegym.book.service.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,12 @@ public class BookController {
     public ResponseEntity<List<Book>>  getList(){
         List<Book> bookList =(List<Book>) bookService.getList();
         return new ResponseEntity<>(bookList, HttpStatus.OK);
+
+    }
+    @PostMapping("/api/books")
+    public ResponseEntity<Void>addBook(@RequestBody Book book){
+        bookService.save(book);
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 
